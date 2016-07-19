7245c1f67b75bf1b7568fc4213ac12cc2cefd69d
BenCJones <ben@bencjones.com>
2016-07-19 15:51:34 -0400

    Completed image chopping class

--- lib/lees_toolbox/tools/images.rb
+++ lib/lees_toolbox/tools/images.rb
@@ -21,6 +21,8 @@ module LeesToolbox
 
     ECI_PATH = "R:/RETAIL/RPRO/Images/Inven"
 
+    COLOR_PROFILE = "C:/WINDOWS/system32/spool/drivers/color/sRGB Color Space Profile.icm"
+
     def initialize(params)
       @source = params[:source]
       @images = piclist(params[:source])
@@ -41,6 +43,9 @@ module LeesToolbox
 
         chopup(image)
 
+        # Log what's left
+        @total -= 1
+        $log.info "#{@total} images left to parse"
       end
     end
 
@@ -57,16 +62,10 @@ module LeesToolbox
       filebase = image.slice(/^[A-Z]{16}/)
       fileattr = image.slice(/(?<=\_)([A-Za-z0-9\_]+)(?=\.)/)
 
-      # We only need to parse to ECI once
-      # So make sure we don't do this group more than once
-      if !@parsed.include?(filebase)
-        @parsed << filebase
-      end
-
       # Check if parsing to ECI
       if @eci
         # If we haven't already done it, output to ECI
-        if @parsed.include?(filebase)
+        if !@parsed.include?(filebase)
           outputs << { size: 350, dest: ECI_PATH, name: "#{filebase}.jpg" }
           outputs << { size: 100, dest: ECI_PATH, name: "#{filebase}t.jpg" }
         end
@@ -82,76 +81,71 @@ module LeesToolbox
       outputs.reverse!
 
       # Create new image object and set defaults
-      imageout = ImageList.new(image_w_path) do
+      imageout = ImageList.new("#{@source}/#{image}") do
         self.background_color = "#ffffff"
         self.gravity = CenterGravity
       end
 
-binding.pry
+      preformat_image!(imageout)
 
-=begin
+      # Chop up image
+      outputs.each do |output|
+        # Resize image
+        imageout.resize!(output[:size],output[:size])
 
+        # Save resized image
+        write_file(imageout, "#{output[:dest]}/#{output[:name]}")
 
-#let's do this later when we've already parsed a large file anyway
-# Add a bare lg file if necessary
-if @formats.include?("large")
-if !@parsed.include?(filebase)
-#          outputs << { size: 
-end
-end
+        # If we need it, save a bare large image
+        if output[:size] == 1050 && !@parsed.include?(filebase)
+          write_file(imageout, "#{output[:dest]}/#{filebase}.jpg")
+        end
 
+        if output[:dest] == "R:/RETAIL/RPRO/Images/Inven"
+          $log.info "Saved to ECI: #{output[:name]}"
+        else
+          $log.info "Saved to dest: #{output[:name]}"
+        end
+        
+      end
 
-      # Preformat imgage
+      # We only need to parse to ECI and large bare files once
+      # So add name to parsed file list
+      if !@parsed.include?(filebase)
+        @parsed << filebase
+      end
+
+      # Kill this whole endealment and clean out garbage
+      imageout.destroy!
+      GC.start
+    end
+
+    # METHOD: Preformats image in advance of chopping it up
+    def preformat_image!(image)
+      @image = image
       # If the image is CMYK, change it to RGB
-      color_profile = "C:/WINDOWS/system32/spool/drivers/color/sRGB Color Space Profile.icm"
-      if image.colorspace == Magick::CMYKColorspace
-        image = image.add_profile(color_profile)
+      if @image.colorspace == Magick::CMYKColorspace
+        @image = @image.add_profile(COLOR_PROFILE)
       end
 
       # If the image has alpha channel transparency, fill it with background color
-      if image.alpha?
-        image.alpha(BackgroundAlphaChannel)
+      if @image.alpha?
+        @image.alpha(BackgroundAlphaChannel)
       end
 
       # If the image size isn't a square, make it a square
-      img_w = image.columns
-      img_h = image.rows
+      img_w = @image.columns
+      img_h = @image.rows
       ratio = img_w.to_f/img_h.to_f
       if ratio < 1
         x = img_h/2-img_w/2
-        image = image.extent(img_h,img_h,x=-x,y=0)
+        @image = @image.extent(img_h,img_h,x=-x,y=0)
       elsif ratio > 1
         y = img_w/2-img_h/2
-        image = image.extent(img_w,img_w,x=0,y=-y)
+        @image = @image.extent(img_w,img_w,x=0,y=-y)
       end
 
-      # Chop up image
-      $outputs.each do |output|
-        # resize(image,size)
-        imgout = image.resize(output[:size],output[:size])
-        write_file(imgout, "#{output[:dest]}/#{output[:name]}")
-        if output[:dest] == "R:/RETAIL/RPRO/Images/Inven"
-          $report << "\n\tSaved to ECI: #{output[:name]}"
-        else
-          $report << "\n\tSaved to dest: #{output[:name]}"
-        end
-        
-        # Killin it
-        imgout.destroy!
-      end
-
-      # Killin it (Part 2)
-      image.destroy!
-      GC.start
-
-      # Reporting
-      if options.verbose
-        puts "\n\n"
-        puts $report << "\n"
-        $total -= 1
-        puts "#{$total} images left to parse\n"
-      end
-=end
+      return self
     end
 
     # METHOD: Returns list of valid image files
