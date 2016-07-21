76a7de9e30f2de40895e25b01e6a8ecbf60bdb89
BenCJones <ben@bencjones.com>
2016-07-21 12:19:37 -0400

    Set up class template

---
+++ lib/lees_toolbox/tools/markdown.rb
@@ -0,0 +1,28 @@
+module LeesToolbox
+
+  def self.run(params)
+    md = Markdown.new(params)
+    md.translate
+  end
+
+  class Markdown
+
+    def initialize(params)
+    
+    
+    
+    
+    end
+
+    def translate
+    
+    end
+
+    private
+  
+  
+  
+  
+  
+  end
+end
