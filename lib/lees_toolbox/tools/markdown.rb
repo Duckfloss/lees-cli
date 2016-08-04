2dc8cb22cce89c0df3e2f2f9990a967d9923d901
BenCJones <ben@bencjones.com>
2016-08-04 15:25:16 -0400

    Text file writing and fix list formatting

--- lib/lees_toolbox/tools/markdown.rb
+++ lib/lees_toolbox/tools/markdown.rb
@@ -79,9 +79,8 @@ module LeesToolbox
         descriptions.each do |row|
           output << format(row)
         end
-      else
-        # Do it with text
-        output = ""
+      elsif @type == ".txt"
+        output = format(data.read)
       end
       output
     end
@@ -89,28 +88,26 @@ module LeesToolbox
     # METHOD: write_to_file(text)
     # write formatted text back to file
     def write_to_file(data)
-      target = File.open(@target, "w")
       if @type == ".csv"
-        CSV.open(@target, "w", :encoding=>"UTF-8", :headers=>true) do |csv|
+        CSV.open(@target, "w", :encoding => "UTF-8", :headers => true) do |csv|
           data.each do |row|
             csv << [row]
           end
         end
       else
-        target << text
-      end
-      if !target.closed?
-        target.close
+        File.open(@target, "w", :encoding => "UTF-8") do |file|
+          file << data
+        end
       end
     end
 
     # METHOD: format
     # Divide text into sections and then filter
-    def format(row)
+    def format(text)
       output = "<ECI>\n<div><font face='verdana'>\n"
       # Divide into hash of sections and
       # Format each section
-      sections = sectionize(row).to_a.map! { |section| filter(section) }
+      sections = sectionize(text).to_a.map! { |section| filter(section) }
 
       # Wrap each section with a div and give it to output
       sections.each do |section|
@@ -169,55 +166,50 @@ module LeesToolbox
     # METHOD: form_of_table(text)
     # Formats block of text as a table
     def form_of_table(text)
-      # Clean up newlines
-      text.gsub!(/\r\n/,"\n")
+      text.gsub!(/\r\n/,"\n")                       # Clean up newlines
       # Figure out what seperator is
       commas = text.scan(",").length
       tabs = text.scan("\t").length
-      commas > tabs ? sep="," : text.gsub!("\t","|"); sep="|"  # Whichever is more is the seperator
-      # Now take out white space
-      text.strip!
-      # Divide text into array of arrays
-      table = text.split("\n").map! { |row| row.split(sep) }
-      # Count rows and columns
-      rows = table.length
+      commas > tabs ? sep="," : text.gsub!("\t","|"); sep="|"   # Whichever is more is the seperator
+      text.strip!                                   # Now take out white space
+      table = text.split("\n").map! { |row| row.split(sep) }    # Divide text into array of arrays
+      rows = table.length                           # Count rows and columns
       columns = 0
       table.each do |row|
         row.length > columns ? columns = row.length : columns
       end
       output = "<table>\n"
       r = 1 # Row counter
-      table.each do |row|                            # Now build table
-        if row.join.length < 1 then next end         # If row is empty then skip
-        output << "\t<tr>\n"                         # Start row
-        c = 0                                        # Column counter
-        colspan = row.length < columns ? columns-row.length+1 : false  # Do we need colspan
-        code = r==1 ? "th" : "td"                    # HTML cell code
+      table.each do |row|                           # Now build table
+        if row.join.length < 1 then next end        # If row is empty then skip
+        output << "\t<tr>\n"                        # Start row
+        c = 0                                       # Column counter
+        colspan = row.length < columns ? columns-row.length+1 : false  # Do we need colspan?
+        code = r==1 ? "th" : "td"                   # HTML cell code
         row.each do |field|
           c += 1
-          output << "\t\t<#{code}"                   # Start tablecell
-          if colspan && c == row.length              # If we're on the last cell, and need a colspan
-            output << " colspan=\"#{colspan}\">"     # Add colspan
-          else                                       # Otherwise
-            output << ">"                            # Just close bracket
+          output << "\t\t<#{code}"                  # Start tablecell
+          if colspan && c == row.length             # If we're on the last cell, and need a colspan
+            output << " colspan=\"#{colspan}\">"    # Add colspan
+          else                                      # Otherwise
+            output << ">"                           # Just close bracket
           end
-          output << sanitize(field)                  # Sanitize text
-          output << "</#{code}>\n"                   # Close tablecell
+          output << sanitize(field)                 # Sanitize text
+          output << "</#{code}>\n"                  # Close tablecell
         end
-        output << "\t</tr>\n"                        # And close row
+        output << "\t</tr>\n"                       # And close row
         r += 1
       end
-      output << "</table>"                           # And close table
+      output << "</table>\n"                        # And close table
     end
 
     # METHOD: form_of_list(text)
     # Formats block of text as a list
     def form_of_list(text)
-      text = sanitize(text)
       output = "<ul>\n"
       listdepth = 1
       # If there are dividers, remove "\n"s
-      text.gsub!(/([:]) *\n+/,"\1 ")
+      text.gsub!(/[:]+[ \n\t]*/,": ")
       text = text.split("\n")
       # Wrap each line in <li>s
       text.each do |line|
@@ -245,6 +237,7 @@ module LeesToolbox
         output << "\t\t</ul>\n\t</li>\n"
       end
       output << "</ul>"
+      sanitize(output)
     end
 
     # METHOD: get_descriptions(data)
@@ -340,186 +333,5 @@ module LeesToolbox
       # Strip off white space on either side
       output.strip
     end
-
-=begin
-# Converts the product description into a hash
-# with the "product_name",
-# "description", "features", and "specs"
-def hashify(string)
-  hash = Hash.new
-  if string != nil
-    string = string.split(/\n(?=\{)/)
-    string.each do |section|
-      hash[ ( section.slice(/[\w\d\_\#]+(?=\})/) ) ] = section[(section.index('}')+1)..-1].strip
-    end
-  end
-  return hash
-end
-
-# encode special characters for HTML
-def html_sanitizer(string, set=:basic)
-  if set == :basic
-    $htmlmap = MAPPINGS[:base]
-  else
-    $htmlmap = MAPPINGS[:base].merge!(MAPPINGS[:title])
-  end
-
-  string = string.split("")
-  string.map! { |char|
-    ( $htmlmap.has_key?(char.unpack('U')[0]) ) ? $htmlmap[char.unpack('U')[0]] : char
-  }
-
-
-  return string.join
-end
-
-# replace \r\n line endings with \n line endings
-# check encoding, if not UTF-8, transcode
-def file_sanitizer(file)
-  file = File.open(file, mode="r+")
-  content = File.read(file)
-	content.force_encoding(Encoding::Windows_1252)
-	content = content.encode!(Encoding::UTF_8, :universal_newline => true)
-  content.gsub!("\r\n","\n")
-  file.write(content)
-end
-
-
-# sanitize and capitalize
-def product_name(string)
-  string = html_sanitizer(title_case(string),:title)
-end
-
-
-# Make it a list
-def listify(string)
-  output = "<ul>\n"
-  string.gsub!(/\:\n/, ":")
-  arrayify = string.split("\n")
-  arrayify.each do |line|
-    line.strip!
-    if line.length>0
-      output << "\t<li>#{line}</li>\n"
-    end
-  end
-  output << "</ul>\n"
-end
-
-# Make it segments
-def segmentify(string)
-  output = "<br>"
-  array = string.split(/\n/)
-  array.each do |x|
-    if x.match(":")
-      output << "#{x}<br>\n"
-    else
-      output << "<strong>#{x}</strong><br>\n"
-    end
-  end
-  return output
-end
-
-# Make it a table
-def tablify(string)
-  output = "<table>\n"
-  r = 0
-  array = string.split(/\n/)
-  array.each do |x|
-    if !x.nil?
-      output << "\t<tr>\n"
-      x.gsub!("  ","\t")
-      row = x.split(/\t/)
-      row.each do |y|
-        if r>0
-          output << "\t\t<td>#{y}</td>\n"
-        else
-          output << "\t\t<th>#{y}</th>\n"
-        end
-      end
-      output << "\t</tr>\n"
-      r += 1
-    end
-  end
-  output << "</table>\n\n"
-  return output
-end
-
-# makes it a paragraph
-def grafify(string)
-  string.strip!
-  string.gsub!("\n","<br>")
-end
-
-
-def format_section(string,format)
-  string=html_sanitizer(string)
-  case format
-  when "table"
-    string = tablify(string)
-  when "seg"
-    string = segmentify(string)
-  when "graf"
-    string = grafify(string)
-  when "list" # is default
-    string=listify(string)
-  else
-    string=listify(string)
-  end
-  return string
-end
-
-
-def formatify(string)
-  output = ""
-  product_data = hashify(string)
-  temp_data = Hash.new
-  product_data.each do |k,v|
-    format = "" # marks what format to put section into
-    if k.match("#")
-      split = k.split("#")
-      k = split[0]
-      format = split[1]
-    end
-
-    case k #checks key
-    when "product_name"
-      temp_data[k]=product_name(v)
-    when "description"
-      temp_data[k] = "<p id=\"description\">#{html_sanitizer(v)}</p>\n"
-    when "features"
-      temp_data[k] = "<p id=\"features\">\n<u>Features</u>\n#{format_section(v,format)}\n</p>\n"
-    when "specs"
-      temp_data[k] = "<p id=\"specifications\">\n<u>Specifications</u>\n#{format_section(v,format)}\n</p>\n"
-    end
-  end
-  output << body_format(temp_data)
-  return output
-end
-
-
-def body_format(hash)
-  product_name = hash["product_name"]
-  description = hash["description"]
-  features = hash["features"]
-  specs = hash["specs"]
-
-  body_format = "<ECI>\n<font face='verdana'>\n"
-  body_format << "<h2 id=\"product_name\">#{hash['product_name']}</h2>\n"
-  if hash.has_key? 'description'
-    body_format << hash['description']
-  end
-  if hash.has_key? 'features'
-    body_format << hash['features']
-  end
-  if hash.has_key? 'specs'
-    body_format << hash['specs']
-  end
-
-  body_format << "</font>"
-
-end
-=end
-
-
   end
 end
